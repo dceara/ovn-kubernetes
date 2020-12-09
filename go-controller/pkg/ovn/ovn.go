00c9797ed25091ff00da2bbf9ca58b157d6a9d61
Dumitru Ceara <dceara@redhat.com>
2020-12-09 14:52:25 +0100

    Add a cluster-wide group with node ls-to-cluster-router ports.
    Commit 40a90f06644a removed the multicast deny port group and used
    instead the cluster port group.  However, this breaks pod-to-pod
    multicast when pods reside on different nodes.  That is because OVN ACLs
    are applied on all logical switch ports, including logical switch ports
    connected to router ports.
    
    Hence, an ACL of the form "if ip.mcast then deny" applied on the
    clusterPortGroup will drop all multicast traffic that would normally be
    routed by the cluster router even when multicast is allowed for a
    namespace.
    
    Instead, add a new (smaller) cluster wide group that only contains the
    node logical switch ports connected to the cluster router.  This allows us
    to define two allow ACLs for multicast traffic from/to node switches to/from
    cluster router, therefore not breaking the namespace multicast allow policy
    if pods reside on different nodes.
    
    Fixes: 40a90f06644a ("Migrate default deny multicast policy to port-group")
    Signed-off-by: Dumitru Ceara <dceara@redhat.com>

--- go-controller/pkg/ovn/ovn.go
+++ go-controller/pkg/ovn/ovn.go
@@ -44,6 +44,7 @@ import (
 const (
 	egressfirewallCRD                string        = "egressfirewalls.k8s.ovn.org"
 	clusterPortGroupName             string        = "clusterPortGroup"
+	clusterRtrPortGroupName          string        = "clusterRtrPortGroup"
 	egressFirewallDNSDefaultDuration time.Duration = 30 * time.Minute
 )
 
@@ -145,6 +146,10 @@ type Controller struct {
 
 	// Port group for all cluster logical switch ports
 	clusterPortGroupUUID string
+
+	// Port group for all node logical switch ports connected to the cluster
+	// logical router
+	clusterRtrPortGroupUUID string
 
 	// Port group for ingress deny rule
 	portGroupIngressDeny string
