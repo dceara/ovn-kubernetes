ca8f91475d4d61ad7a78c66b46cece9a7e5e0698
Dumitru Ceara <dceara@redhat.com>
2024-12-11 10:05:54 +0100

    HACK: enable mutex profiling
    Signed-off-by: Dumitru Ceara <dceara@redhat.com>

--- go-controller/cmd/ovnkube/ovnkube.go
+++ go-controller/cmd/ovnkube/ovnkube.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"os"
 	"os/signal"
+	"runtime"
 	"strings"
 	"sync"
 	"syscall"
@@ -428,6 +429,7 @@ func runOvnKube(ctx context.Context, runMode *ovnkubeRunMode, ovnClientset *util
 			err = fmt.Errorf("recovering from a panic in runOvnKube: %v", r)
 		}
 	}()
+	runtime.SetMutexProfileFraction(1)
 	startTime := time.Now()
 
 	if runMode.cleanupNode {
